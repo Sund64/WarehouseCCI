d8e49ff721143e626286ed6a69b730c6054fae4c
Sund64 <chris.cornelis.lukito@gmail.com>
2026-07-14 23:36:45 +0700

    Revisi V1

--- config/db.js
+++ config/db.js
@@ -1,7 +1,6 @@
 const mysql = require('mysql2');
 require('dotenv').config();
 
-// Buat pool koneksi seperti biasa
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
